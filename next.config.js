a7c7a78a05c91e53936a8102b7e9df4e723845ea
Sanjay Achar <sanjay@bobthecoder.org>
2021-10-29 10:37:34 +0530

    fixing sentry build error

--- next.config.js
+++ next.config.js
@@ -6,25 +6,35 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
-  // your existing module.exports
-  reactStrictMode: true,
-  target: 'serverless',
-  sentry: {
-    disableServerWebpackPlugin: true,
-    disableClientWebpackPlugin: true,
-  },  
+	// your existing module.exports
+	reactStrictMode: true,
+	target: 'experimental-serverless-trace',
+	sentry: {
+		disableServerWebpackPlugin: true,
+		disableClientWebpackPlugin: true,
+	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			// don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+			config.resolve.fallback = {
+				fs: false
+			}
+		}
+
+		return config;
+	}
 };
 
 const SentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+	// Additional config options for the Sentry Webpack plugin. Keep in mind that
+	// the following options are set automatically, and overriding them is not
+	// recommended:
+	//   release, url, org, project, authToken, configFile, stripPrefix,
+	//   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
+	silent: true, // Suppresses all logs
+	// For all available options, see:
+	// https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
