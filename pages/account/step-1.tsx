f8807474a885df2dfb21c029fa3778cf98b4ce5d
Sanjay Achar <sanjay@knowhere.studio>
2021-12-12 16:43:43 +0000

    step-2 accounts page is completed

--- pages/account/step-1.tsx
+++ pages/account/step-1.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AccountLayout from '../../components/layouts/AccountLayout';
 import Input from '../../components/shared/Input';
+import * as Yup from 'yup';
+import { Formik, FormikHelpers } from 'formik';
 
 type Props = {};
 type State = {
-    firstName: string,
-    lastName: string,
+    first_name: string,
+    last_name: string,
     email: string,
     phone: string,
     location: string
@@ -13,25 +15,39 @@ type State = {
 
 class Step1Page extends React.PureComponent<Props, State> {
 
-    state: State = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        phone: "",
-        location: ""
-    }
-
-    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target
-        if (name === "phone") {
-            if (/^\d+$/.test(value) || value === "")
-                this.setState({ [name]: value } as Pick<State, keyof State>)
-        }
-        else
-            this.setState({ [name]: value } as Pick<State, keyof State>)
+    handleSubmit = (values: State, helper: FormikHelpers<State>): void => {
+        console.log(values)
     }
 
     render(): JSX.Element {
+        const Form = {
+            InitialValue: {
+                first_name: '',
+                last_name: '',
+                email: '', 
+                phone: '',
+                location: '',
+            },
+            Validation: Yup.object({
+                first_name: Yup.string()
+                    .matches(/^[a-zA-Z ]*$/, 'First name should only container alphabets')
+                    .min(4, 'Too short!')
+                    .required('First Name is mandatory'),
+                last_name: Yup.string()
+                    .matches(/^[a-zA-Z ]*$/, 'First name should only container alphabets')
+                    .min(4, 'Too short!')
+                    .required('Last Name is mandatory'),
+                email: Yup.string()
+                    .email('Enter a valid email address')
+                    .required('Email address is mandatory'),
+                phone: Yup.string()
+                    .matches(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/, 'Enter a valid phone number')
+                    .required('Phone number is mandatory'),
+                location: Yup.string()
+                    .matches(/^[a-zA-Z0-9\s,'-]*$/, 'Enter a valid address')
+                    .required('Location is mandatory')
+            })
+        }
         return (
             <AccountLayout>
                 {/* 
@@ -48,120 +64,83 @@ class Step1Page extends React.PureComponent<Props, State> {
                             d) error text (if not null, the input change style as per the design)
 
                     2. Formik integrations
+
+
+                    ------
+                    @ cleanup - unused html & styles
+                    @ Formik & yup integrations
                 */}
                 <h1 className="personal-details text-center mb-5">
                     Personal Details
                 </h1>
-                <form className="user-details needs-validation" noValidate onSubmit={(e) => { e.preventDefault() }}>
-                    <Input
-                        name="first_name"
-                        label={'First Name'}
-                        type={'text'}
-                        autoComplete={'off'}
-                        placeholder={'Enter your first name'}
-                        value={'Sanjay'}
-                    />
+                <Formik 
+                    initialValues={Form.InitialValue} 
+                    validationSchema={Form.Validation} 
+                    onSubmit={(value, helper) => this.handleSubmit(value, helper)}  
+                >
+                    {
+                        (props) => (
+                            <form className="user-details needs-validation" noValidate onSubmit={props.handleSubmit}>
+                                <Input
+                                    name="first_name"
+                                    label={'First Name'}
+                                    type={'text'}
+                                    autoComplete={'off'}
+                                    placeholder={'Enter your first name'}   
+                                    value={props.values.first_name}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.first_name !== undefined}
+                                />
 
-                    <Input
-                        name="last_name"
-                        label={'Last Name'}
-                        type={'text'}
-                        autoComplete={'off'}
-                        placeholder={'Enter your last name'}
-                    />
+                                <Input
+                                    name="last_name"
+                                    label={'Last Name'}
+                                    type={'text'}
+                                    autoComplete={'off'}
+                                    placeholder={'Enter your last name'}
+                                    value={props.values.last_name}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.last_name !== undefined}
+                                />
 
-                    <Input
-                        name="email"
-                        label={'Email address'}
-                        type={'email'}
-                        autoComplete={'off'}
-                        placeholder={'Enter your email address'}
-                        hasError
-                    /> 
+                                <Input
+                                    name="email"
+                                    label={'Email address'}
+                                    type={'email'}
+                                    autoComplete={'off'}
+                                    placeholder={'Enter your email address'}
+                                    value={props.values.email}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.email !== undefined}
+                                /> 
 
-                    <Input
-                        name="phone"
-                        label={'Phone number'}
-                        type={'tel'}
-                        autoComplete={'off'}
-                        placeholder={'Enter your phone number'}
-                    />                        
+                                <Input
+                                    name="phone"
+                                    label={'Phone number'}
+                                    type={'tel'}
+                                    autoComplete={'off'}
+                                    placeholder={'Enter your phone number'}
+                                    value={props.values.phone}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.phone !== undefined}
+                                />                        
 
-                    <Input
-                        name="location"
-                        label={'Location'}
-                        type={'text'}
-                        autoComplete={'off'}
-                        placeholder={'Enter your location'}
-                    />                                                                           
-
-                    {/* <div className="mb-4 form-group">
-                        <label className="user-details-labels">
-                            <h6 className="ml-lg-3 mb-0">First name</h6>
-                            <input type="text" 
-                                value={this.state.firstName}
-                                className="form-control user-inputs" 
-                                name="firstName" 
-                                placeholder="Enter your first name" 
-                                required 
-                                onChange={this.handleChange}/>
-                        </label>
-                    </div>
-                    <div className="mb-4 form-group">
-                        <label className="user-details-labels">
-                            <h6 className="ml-lg-3 mb-0">Last name</h6>
-                            <input 
-                                value={this.state.lastName}
-                                type="text" 
-                                className="form-control user-inputs" 
-                                name="lastName" 
-                                placeholder="Enter your last name" 
-                                required 
-                                onChange={this.handleChange}/>
-                        </label>
-                    </div>
-                    <div className="mb-4 form-group">
-                        <label className="user-details-labels">
-                            <h6 className="ml-lg-3 mb-0">Email address</h6>
-                            <input 
-                                value={this.state.email}
-                                type="email" 
-                                className="form-control user-inputs" 
-                                name="email" 
-                                placeholder="Enter your email" 
-                                required 
-                                onChange={this.handleChange}/>
-                        </label>
-                    </div>
-                    <div className="mb-4 form-group">
-                        <label className="user-details-labels">
-                            <h6 className="ml-lg-3 mb-0">Phone</h6>
-                            <input 
-                                value={this.state.phone}
-                                type="text" 
-                                maxLength={10} 
-                                className="form-control user-inputs" 
-                                name="phone"
-                                placeholder="Enter your phone number" 
-                                required 
-                                onChange={this.handleChange}/>
-                        </label>
-                    </div>
-                    <div className="mb-3 form-group">
-                        <label className="user-details-labels">
-                            <h6 className="ml-lg-3 mb-0">Location</h6>
-                            <input 
-                                value={this.state.location}
-                                type="text" 
-                                className="form-control user-inputs" 
-                                name="location" 
-                                placeholder="Enter your current location" 
-                                required 
-                                onChange={this.handleChange}/>
-                        </label>
-                    </div> */}
-                    <button type="submit" className="btn next-btn w-100 mt-2">Next</button>
-                </form>
+                                <Input
+                                    name="location"
+                                    label={'Location'}
+                                    type={'text'}
+                                    autoComplete={'off'}
+                                    placeholder={'Enter your location'}
+                                    value={props.values.location}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.location !== undefined}
+                                />                                                         
+                                <button type="submit" className="btn btn-primary w-100 mt-2">Next</button>
+                                {console.log(props.errors)}
+                            </form>
+                        )
+                    }
+                </Formik>
             </AccountLayout>
         )
     }
--- pages/account/step-2.tsx
+++ pages/account/step-2.tsx
@@ -1,14 +1,115 @@
 import React from 'react';
 import AccountLayout from '../../components/layouts/AccountLayout';
+import Input from '../../components/shared/Input';
+import * as Yup from 'yup';
+import { Formik, FormikHelpers } from 'formik';
 
 type Props = {};
-type State = {};
+type State = {
+    stackoverflow_username: string,
+    github_username: string,
+    your_website: string,
+    public_email: string,
+};
 
 class Step2Page extends React.PureComponent<Props, State> {
+
+    handleSubmit = (values: State, helper: FormikHelpers<State>): void => {
+        console.log(values)
+    }
+    
     render(): JSX.Element {
+        const Form = {
+            InitialValue: {
+                stackoverflow_username: '',
+                github_username: '',
+                your_website: '',
+                public_email: '',
+            },
+            Validation: Yup.object({
+                // (?:https?:)?\/\/(?:www\.)?stackoverflow\.com\/users\/[0-9]+\/[A-z0-9-_.]+\/?
+                stackoverflow_username: Yup.string()
+                    .matches( /(?:https?:)?\/\/(?:www\.)?stackoverflow\.com\/users\/[0-9]+\/[A-z0-9-_.]+\/?/, "Enter a valid stackoverflow username")
+                    .required("Stackoverflow username is mandatory"),
+                
+                    // (?:https?:)?\/\/(?:www\.)?github\.com\/[A-z0-9-_.]+\/?
+                github_username: Yup.string()
+                    .matches(/(?:https?:)?\/\/(?:www\.)?github\.com\/[A-z0-9-_.]+\/?/, "Enter a valid github username")
+                    .required("Github username is mandatory"),
+                
+                // https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
+
+                your_website: Yup.string()
+                    .matches(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, "Enter a valid domain name")
+                    .required("Your website link is mandatory"),
+                    
+                public_email: Yup.string()
+                    .email('Enter a valid email address')
+                    .required('Email address is mandatory')
+            })
+        }
+
         return (
             <AccountLayout>
-                Hello, world!
+                <h1 className="personal-details text-center mb-5">
+                    Personal Details
+                </h1>
+                <Formik 
+                    initialValues={Form.InitialValue} 
+                    validationSchema={Form.Validation} 
+                    onSubmit={(value, helper) => this.handleSubmit(value, helper)}  
+                >
+                    {
+                        (props) => (
+                            <form className="user-details needs-validation" noValidate onSubmit={props.handleSubmit}>
+                                <Input
+                                    name="stackoverflow_username"
+                                    label={'Stackoverflow Username'}
+                                    type={'text'}
+                                    autoComplete={'off'}
+                                    placeholder={'Your stackoverflow profile link'}   
+                                    value={props.values.stackoverflow_username}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.stackoverflow_username!== undefined}
+                                />
+
+                                <Input
+                                    name="github_username"
+                                    label={'Github Username'}
+                                    type={'text'}
+                                    autoComplete={'off'}
+                                    placeholder={'Your github profile link'}
+                                    value={props.values.github_username}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.github_username !== undefined}
+                                />
+
+                                <Input
+                                    name="your_website"
+                                    label={'Your Website'}
+                                    type={'text'}
+                                    autoComplete={'off'}
+                                    placeholder={'Your website link'}
+                                    value={props.values.your_website}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.your_website !== undefined}
+                                /> 
+
+                                <Input
+                                    name="public_email"
+                                    label={'Public email address'}
+                                    type={'email'}
+                                    autoComplete={'off'}
+                                    placeholder={'Your public email address'}
+                                    value={props.values.public_email}
+                                    onChange={props.handleChange}
+                                    hasError={props.errors.public_email !== undefined}
+                                />                                                                             
+                                <button type="submit" className="btn btn-primary w-100 mt-2">Create</button>
+                            </form>
+                        )
+                    }
+                </Formik>            
             </AccountLayout>
         )
     }
